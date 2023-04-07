330ea9f7f712d06eceace0bbf4ff422804e3f3bd
pablocastro <metal145965@live.com>
2023-04-07 11:21:39 -0500

    added tic tac toe

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from rps import rps
 from guessing_game import guessing_game
+from tic_tac_toe import play_t3
 
 def main():
-    print("Available games:")
-    print("1. Rock, Paper, Scissors")
-    print("2. Number Guessing Game")
-    print("3. Tic Tac Toe")
-    print("4. Connect Four")
+    while True:
+        print("Available games:")
+        print("1. Rock, Paper, Scissors")
+        print("2. Number Guessing Game")
+        print("3. Tic Tac Toe")
+        print("4. Exit")
 
-    game_choice = input("What game would you like to play? ")
+        game_choice = input("What game would you like to play? ")
 
-    if game_choice == "1":
-        rps()
-    elif game_choice == "2":
-        guessing_game()
-    elif game_choice == "3":
-        pass
-    elif game_choice == "4":
-        pass
-    else:
-        print("Invalid input. Please enter a number between 1 and 4.")
-        main()
+        if game_choice == "1":
+            rps()
+        elif game_choice == "2":
+            guessing_game()
+        elif game_choice == "3":
+            play_t3()
+        elif game_choice == "4":
+            exit()
+        else:
+            print("Invalid input. Please enter a number between 1 and 4.")
+            main()
 
 if __name__ == "__main__":
     main()
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,12 @@
-BOARD = {1: '7',  2: '8',  3: '9',
+import random
+
+BOARD = {7: '7',  8: '8',  9: '9',
 
          4: '4',  5: '5',  6: '6',
 
-         7: '1',  8: '2',  9: '3'}
+         1: '1',  2: '2',  3: '3'}
+
+mode = None
 
 
 def render():
@@ -25,14 +29,14 @@ def render():
     BOARD : dict
     '''
 
-    print(f" {BOARD[1]} | {BOARD[2]} | {BOARD[3]} ")
+    print(f" {BOARD[7]} | {BOARD[8]} | {BOARD[9]} ")
     print(" - + - + - ")
     print(f" {BOARD[4]} | {BOARD[5]} | {BOARD[6]} ")
     print(" - + - + - ")
-    print(f" {BOARD[7]} | {BOARD[8]} | {BOARD[9]} ")
+    print(f" {BOARD[1]} | {BOARD[2]} | {BOARD[3]} ")
 
 
-def get_action(player):
+def get_action(player, available_moves):
     '''
     Prompts the current player for a number between 1 and 9.
     Checks* the returning input to ensure that it is an integer
@@ -57,8 +61,19 @@ def get_action(player):
     *Note: Implementing a while loop in this function is recommended,
     but make sure you aren't coding any infinite loops.
     '''
-    # Get user input and assign it to a variable called 'action'.
+    if player == 'O' and mode == 'S':
+        print(f"Computer is choosing a move...")
+        action = available_moves[random.randint(0, len(available_moves) - 1)]
+        available_moves.remove(action)
+    else:
+        action = int(input(
+            f"Player {player}, please choose a number between 1 and 9 to indicate where to place your move: "))
+        while action not in available_moves:
+            action = int(input(
+                f"Player {player}, please choose a number between 1 and 9 to indicate where to place your move: "))
+        available_moves.remove(action)
 
+    return action
 
 
 def victory_message(player):
@@ -78,9 +93,18 @@ def victory_message(player):
     ---------------------
     print_t3() : func
     '''
-    # ----------------
-    # INSERT CODE HERE
-    # ----------------
+    print(f"Player {player} wins!")
+    render()
+    with open('log.txt', 'a') as f:
+        f.write("------------------------\n")
+        f.write("TicTacToe\n")
+        f.write("------------------------\n")
+        f.write(f"Player {player} wins!\n")
+        f.write(f" {BOARD[7]} | {BOARD[8]} | {BOARD[9]} \n")
+        f.write(" - + - + - \n")
+        f.write(f" {BOARD[4]} | {BOARD[5]} | {BOARD[6]} \n")
+        f.write(" - + - + - \n")
+        f.write(f" {BOARD[1]} | {BOARD[2]} | {BOARD[3]} \n")
 
 
 def check_win(player):
@@ -103,9 +127,54 @@ def check_win(player):
     BOARD : dict
     victory_message(player) : func
     '''
-    # ----------------
-    # INSERT CODE HERE
-    # ----------------
+    winning_combinations = [
+        [7, 8, 9], [4, 5, 6], [1, 2, 3],
+        [7, 4, 1], [8, 5, 2], [9, 6, 3],
+        [1, 5, 9], [3, 5, 7]
+    ]
+
+    if any(all(BOARD[position] == player for position in combination) for combination in winning_combinations):
+        victory_message(player)
+        return True
+
+    return False
+
+
+def player_turn(player, game_round, available_moves):
+    # Print the current state of the board
+    render()
+
+    # Get the current player's action and assign it to a variable called 'action'.
+    action = get_action(player, available_moves)
+
+    # Assign the current player ('X' or 'O') as a value to BOARD. Use the 'action' variable as the key.
+    BOARD[action] = player
+
+    # Check if the game is winnable (game_round >= 4),
+    # then check for win conditions (check_win(player)),
+    # and if there's a win, end the game (game_over = True),
+    # and break the loop (break).
+    if game_round >= 4:
+        if check_win(player):
+            return True
+
+    # Check if there are any open spots left (game_round == 9),
+    # and if there aren't, print a tie message,
+    # end the game,
+    # and break the loop.
+    if len(available_moves) == 0:
+        print("It's a tie!")
+        with open('log.txt', 'a') as f:
+            f.write("------------------------\n")
+            f.write("TicTacToe\n")
+            f.write("------------------------\n")
+            f.write("It's a tie!")
+            f.write(f" {BOARD[7]} | {BOARD[8]} | {BOARD[9]} \n")
+            f.write(" - + - + - \n")
+            f.write(f" {BOARD[4]} | {BOARD[5]} | {BOARD[6]} \n")
+            f.write(" - + - + - \n")
+            f.write(f" {BOARD[1]} | {BOARD[2]} | {BOARD[3]} \n")
+        return True
 
 
 def play_t3():
@@ -124,37 +193,39 @@ def play_t3():
     inescapable infinite loops.
     '''
 
-    player = 'X'
+    player_1 = 'X'
+    player_2 = 'O'
     game_round = 0
     game_over = False
+    available_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+    # Ask for mode selection
+    global mode
+    mode = input(
+        "Would you like to play against a single-player or the multi-layer? (S/M) ").upper()
+    while mode != "S" and mode != "M":
+        print("Invalid input. Please enter 'S' or 'M'.")
+        mode = input(
+            "Would you like to play against a single-player or the multi-layer? (S/M) ")
+
+    # Game loop
     while not game_over:
+        game_round += 1
+        if player_turn(player_1, game_round, available_moves):
+            break
 
-        game_over = True  # Delete this line when you're ready to run the loop.
-
-        # Print the current state of the board
-        render()
-
-        # Get the current player's action and assign it to a variable called 'action'.
-
-        # Assign the current player ('X' or 'O') as a value to BOARD. Use the 'action' variable as the key.
-
-        # Increment the game round by 1.
-
-        # Check if the game is winnable (game_round >= 4),
-        # then check for win conditions (check_win(player)),
-        # and if there's a win, end the game (game_over = True),
-        # and break the loop (break).
-
-        # Check if there are any open spots left (game_round == 9),
-        # and if there aren't, print a tie message,
-        # end the game,
-        # and break the loop.
-
-        # switch players with a quick conditional loop.
+        game_round += 1
+        if player_turn(player_2, game_round, available_moves):
+            break
 
     # prompt for a restart and assign the input to a 'restart' variable.
-    # if yes,
-        # clear each key in the board with a for loop
-
-        # and reinitiate the game loop (play_t3()).
+    restart = input("Would you like to play again? (Y/N) ").upper()
+    while restart != "Y" and restart != "N":
+        restart = input("Would you like to play again? (Y/N) ")
+    if restart == "Y":
+        for key in BOARD:
+            BOARD[key] = key
+        play_t3()
+    else:
+        print("Thanks for playing!")
+        return
